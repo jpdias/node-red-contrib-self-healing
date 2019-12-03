da52258b78bd21ae72d0de904501b5845f24dad9
jpdias <jpdias@live.com.pt>
2019-12-03 13:55:03 +0000

    redundancy WIP

--- redundancy/redundancy.js
+++ redundancy/redundancy.js
@@ -1,25 +1,31 @@
 module.exports = function(RED) {
+    /*get my ip stuff*/
     const os = require("os");
-
-    var masterExists = false;
     function getIp() {
         let ips = [];
-
         var interfaces = os.networkInterfaces();
-
         for (var i in interfaces) {
             interfaces[i].forEach(function(details) {
                 details.interface = i;
                 if (!details.internal) {
-                    delete details.netmask;
-                    delete details.mac;
-                    delete details.internal;
-
-                    ips.push(details);
+                    ips.push(details.address);
                 }
             });
         }
-        return ips[0].address;
+        return ips[0];
+    }
+
+    /*setMaster if none exists*/
+    var masterExists = false;
+    var syncPayload = {"payload": {"sync": "ping", "master": false}};
+
+    function getMajor(res){
+        return res.reduce(function(a, b) {
+            return Math.max(
+                Array.from(a.split('.')).map(item => parseInt(item)).reduce((a, b) => a + b, 0),
+                Array.from(b.split('.')).map(item => parseInt(item)).reduce((a, b) => a + b, 0),
+            );
+        });
     }
 
     function setMaster(node, send, done) {
@@ -33,9 +39,7 @@ module.exports = function(RED) {
         let res = Array.from(node.context().get("ips"));
         let major = null;
         if(res.lenght >= 0){
-            major = res.reduce(function(a, b) {
-                return Math.max(parseInt(a.slice(-2)), parseInt(b.slice(-2)));
-            });
+            major = getMajor(res)
         }
         
         if (res.lenght == 0 &&
@@ -48,28 +52,47 @@ module.exports = function(RED) {
             !node.context().global.get("master") &&
             !masterExists
         ) {
-            node.context().set("master", true);
+            node.context().global.set("master", true);
             isMaster = true;
         }
-
-        send([{ "payload": {"master": isMaster} }, { "payload": res }]);
+        syncPayload.payload.master = node.context().global.get("master");
+        send([{ "payload": {"master": isMaster} }, { "payload": res }, syncPayload]);
         done();
     }
 
     function RedundancyManager(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        var timeout = "undefined";
+        node.context().global.set("master", false);
+
+        let interval = parseInt(config.pingInterval) < 15 ? 15 : 30;
+
+        node.emit("input", syncPayload);
+
+        setInterval(() => {
+            node.emit("input", syncPayload);
+        }, interval*1000);
+
         node.on("input", function(msg, send, done) {
-            setTimeout(
+
+            if (timeout == "undefined"){
+                timeout = setTimeout(
                 setMaster,
                 parseInt(config.timeout) * 1000,
                 node,
                 send,
                 done
-            );
-            if(!msg.hostip){
-                node.status({ fill: "red", shape: "ring", text: "Error in input" });
-                return;
+                );
+            }
+            if(node.context().global.get("master")){
+                node.status({ fill: "green", shape: "ring", text: "I'm Master"}); 
+            }
+            else if (msg.payload.master == "true") {
+                masterExists = true;
+                node.context().global.set("master", false);
+                clearTimeout(timeout);
+                node.status({ fill: "yellow", shape: "ring", text: "I'm Slave. Master is "+ msg.hostip});
             }
             if (!node.context().get("ips")) {
                 let ips = new Set([msg.hostip]);
@@ -82,10 +105,12 @@ module.exports = function(RED) {
                     node.context().set("ips", nodes);
                 }
             }
-            if (msg.payload.master == "true") {
-                masterExists = true;
+            if(msg.payload.sync == "ping"){
+                syncPayload.payload.master = node.context().global.get("master");
+                send([null, null, syncPayload]);
+                node.status({ fill: "green", shape: "ring", text: "Sync Ping"});
+                done();
             }
-            
         });
     }
 
