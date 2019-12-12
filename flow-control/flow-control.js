a5e6a96f5a51dc8ef36a2e8392fa2b849c8f0f90
jpdias <jpdias@live.com.pt>
2019-12-12 18:54:21 +0000

    refactor flow control

--- flow-control/flow-control.js
+++ flow-control/flow-control.js
@@ -1,59 +1,116 @@
 var request = require("request");
 
 module.exports = function(RED) {
-  function FlowControl(config) {
-    RED.nodes.createNode(this, config);
-    var node = this;
-    node.on("input", function(msg, send, done) {
-      let targetUrl = `http://${config.targetHost}:${config.targetPort}/flow/${config.targetFlow}`;
-
-      if (typeof msg.payload === "boolean") {
-        try {
-          request.get(targetUrl, function(error, response, body) {
-            if (error) {
-              node.status({ fill: "red", shape: "ring", text: "Error" });
-              done(error);
-            }
-            let editedFlow = JSON.parse(body);
-            editedFlow["disabled"] = !msg.payload ? true : false;
-            request.put(
-              {
-                method: "PUT",
-                uri: targetUrl,
-                json: editedFlow
-              },
-              function(error, response, body) {
-                if (error) {
-                  node.status({ fill: "red", shape: "ring", text: "Error" });
-                  node.send([null, { payload: "error" }]);
-                  done(error);
-                } else {
-                  node.status({ fill: "green", shape: "ring", text: "Ok" });
-                  node.send([{
-                    payload: {
-                      id: config.targetFlow,
-                      disabled: editedFlow["disabled"]
+    function setFlowStatus(targetUrl, editedFlow, node, done, config) {
+        request
+            .put(
+                {
+                    method: "PUT",
+                    uri: targetUrl,
+                    json: editedFlow
+                },
+                function(error, response, body) {
+                    if (error) {
+                        node.status({
+                            fill: "red",
+                            shape: "ring",
+                            text: "Error"
+                        });
+                        node.send([null, { payload: "error" }]);
+                        done(error);
+                    } else {
+                        node.status({
+                            fill: "green",
+                            shape: "ring",
+                            text: "Ok"
+                        });
+                        node.send([
+                            {
+                                payload: {
+                                    id: config.targetFlow,
+                                    disabled: editedFlow["disabled"]
+                                }
+                            },
+                            null
+                        ]);
+                        done();
                     }
-                  }, null]);
-                  done();
+                    console.log(
+                        "Upload successful!  Server responded with:",
+                        body
+                    );
                 }
-                console.log("Upload successful!  Server responded with:", body);
-              }
-            );
-          });
-        } catch (error) {
-          if (error) {
-            node.status({
-              fill: "red",
-              shape: "ring",
-              text: "Node Missconfig"
+            )
+            .on("error", function(err) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "Node Missconfig"
+                });
+                done(err);
             });
-            done(error);
-          }
-        }
-      }
-    });
-  }
+    }
 
-  RED.nodes.registerType("flow-control", FlowControl);
+    function getSetFlowStatus(
+        targetUrl,
+        node,
+        done,
+        msg,
+        setFlowStatus,
+        config
+    ) {
+        request
+            .get(targetUrl, function(error, response, body) {
+                if (error) {
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: "Error"
+                    });
+                    done(error);
+                }
+                let editedFlow = JSON.parse(body);
+                editedFlow["disabled"] = !msg.payload ? true : false;
+                setFlowStatus(targetUrl, editedFlow, node, done, config);
+            })
+            .on("error", function(err) {
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "Node Missconfig"
+                });
+                done(err);
+            });
+    }
+    function FlowControl(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on("input", function(msg, send, done) {
+            let targetUrl = `http://${config.targetHost}:${config.targetPort}/flow/${config.targetFlow}`;
+
+            if (typeof msg.payload === "boolean") {
+                try {
+                    getSetFlowStatus(
+                        targetUrl,
+                        node,
+                        done,
+                        msg,
+                        setFlowStatus,
+                        config
+                    );
+                } catch (error) {
+                    if (error) {
+                        node.status({
+                            fill: "red",
+                            shape: "ring",
+                            text: "Node Missconfig"
+                        });
+                        done(error);
+                    }
+                }
+            }
+        });
+    }
+
+    RED.nodes.registerType("flow-control", FlowControl);
 };
