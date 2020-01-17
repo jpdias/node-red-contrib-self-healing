1f47f6f09ffb96a71468992568640ec3c74f4d1d
jpdias <jpdias@live.com.pt>
2020-01-17 19:20:52 +0000

    distinct node

---
+++ distinct/distinct.js
@@ -0,0 +1,21 @@
+module.exports = function (RED) {
+    function Distinct(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var lastMsg;
+        node.on("input", function (msg, send, done) {
+            if(typeof lastMsg == "undefined" || lastMsg != msg.payload){
+                lastMsg = msg.payload;
+                node.status({ fill: "green", shape: "ring", text: "Distinct" });
+                send([msg, null]);
+                done();
+            } else {
+                node.status({ fill: "red", shape: "ring", text: "Repeated" });
+                send([null, msg]);
+                done();
+            }
+        });
+    }
+
+    RED.nodes.registerType("distinct", Distinct);
+};
