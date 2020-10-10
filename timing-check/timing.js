30b8349e782e8b8ff952527f67b585ce9991e6a1
João N. Matos <joao.nuno.matos@fe.up.pt>
2020-10-10 12:32:13 +0100

    style: run format on entire project
    Self explanatory. Formatting was inconsistent,
    we now have a formatter, ran it on the entire repo.

--- timing-check/timing.js
+++ timing-check/timing.js
@@ -1,57 +1,55 @@
 module.exports = function (RED) {
+  function Timing(config) {
+    RED.nodes.createNode(this, config);
+    this.lastTimestamp = null;
+    this.repeat = config.frequency * 1000; // from seconds to milis
+    this.margin = config.margin;
+    this.max = this.repeat * (1 + this.margin);
+    this.min = this.repeat * (1 - this.margin);
+    var node = this;
 
-    function Timing(config) {
-        RED.nodes.createNode(this, config);
-        this.lastTimestamp = null;
-        this.repeat = config.frequency * 1000; // from seconds to milis
-        this.margin = config.margin;
-        this.max = this.repeat * (1 + this.margin);
-        this.min = this.repeat * (1 - this.margin);
-        var node = this;
-
-        this.on("input", function (msg) {
-            let nowtime = Date.now();
-            if (!this.lastTimestamp) {
-                this.lastTimestamp = Date.now();
-                msg.timestamp = this.lastTimestamp;
-                node.status({
-                    fill: "green",
-                    shape: "dot",
-                    text: "First msg"
-                });
-                node.send([msg, null, null]);
-            } else {
-                let difference = nowtime - this.lastTimestamp;
-                if (difference < this.max && difference > this.min) {
-                    node.status({
-                        fill: "green",
-                        shape: "dot",
-                        text: "Ok"
-                    });
-                    msg.timestamp = nowtime;
-                    node.send([msg, null, null]);
-                } else if (difference < this.max) {
-                    node.status({
-                        fill: "yellow",
-                        shape: "dot",
-                        text: "Too fast"
-                    });
-                    msg.timestamp = nowtime;
-                    node.send([null, msg, null]);
-                } else if (difference > this.min) {
-                    node.status({
-                        fill: "yellow",
-                        shape: "dot",
-                        text: "Too slow"
-                    });
-                    msg.timestamp = nowtime;
-                    node.send([null, null, msg]);
-                }
-
-            }
-            this.lastTimestamp = nowtime;
+    this.on("input", function (msg) {
+      let nowtime = Date.now();
+      if (!this.lastTimestamp) {
+        this.lastTimestamp = Date.now();
+        msg.timestamp = this.lastTimestamp;
+        node.status({
+          fill: "green",
+          shape: "dot",
+          text: "First msg",
         });
-    }
+        node.send([msg, null, null]);
+      } else {
+        let difference = nowtime - this.lastTimestamp;
+        if (difference < this.max && difference > this.min) {
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: "Ok",
+          });
+          msg.timestamp = nowtime;
+          node.send([msg, null, null]);
+        } else if (difference < this.max) {
+          node.status({
+            fill: "yellow",
+            shape: "dot",
+            text: "Too fast",
+          });
+          msg.timestamp = nowtime;
+          node.send([null, msg, null]);
+        } else if (difference > this.min) {
+          node.status({
+            fill: "yellow",
+            shape: "dot",
+            text: "Too slow",
+          });
+          msg.timestamp = nowtime;
+          node.send([null, null, msg]);
+        }
+      }
+      this.lastTimestamp = nowtime;
+    });
+  }
 
-    RED.nodes.registerType("timing", Timing);
-}
+  RED.nodes.registerType("timing", Timing);
+};
