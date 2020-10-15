19b82f0b1c16fb200dbc690e62eb8c4785e1103c
SALajes <sofialajes98@gmail.com>
2020-10-15 21:13:29 +0100

    feat: timing check node reimplemented

--- timing-check/timing.js
+++ timing-check/timing.js
@@ -1,56 +1,64 @@
 module.exports = function (RED) {
-
     function Timing(config) {
         RED.nodes.createNode(this, config);
-        this.lastTimestamp = null;
-        this.repeat = config.frequency * 1000; // from seconds to milis
-        this.margin = config.margin;
-        this.max = this.repeat * (1 + this.margin);
-        this.min = this.repeat * (1 - this.margin);
-        var node = this;
 
-        this.on("input", function (msg) {
-            let nowtime = Date.now();
-            if (!this.lastTimestamp) {
-                this.lastTimestamp = Date.now();
-                msg.timestamp = this.lastTimestamp;
+        this.lastTimestamp = null;
+        this.periodBetweenReadings = config.period * 1000; //seconds to milliseconds
+        this.intervalMargin = config.margin;
+
+        this.maximumPeriod = this.periodBetweenReadings + this.intervalMargin*this.periodBetweenReadings;
+        this.minimumPeriod = this.periodBetweenReadings - this.intervalMargin*this.periodBetweenReadings;
+
+        let node = this;
+
+        this.on('input', (msg) => {
+            let currentTimestamp = Date.now();
+
+            if(this.lastTimestamp == null){
+                this.lastTimestamp = currentTimestamp;
+                msg.timestamp = currentTimestamp;
+
                 node.status({
                     fill: "green",
                     shape: "dot",
-                    text: "First msg"
+                    text: "First message"
                 });
+
                 node.send([msg, null, null]);
-            } else {
-                let difference = nowtime - this.lastTimestamp;
-                if (difference < this.max && difference > this.min) {
+            }
+            else{
+                let intervalPeriod = currentTimestamp - this.lastTimestamp;
+
+                if(intervalPeriod > this.maximumPeriod){
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: "Too Fast"
+                    });
+    
+                    node.send([null, null, msg]);
+                }
+                else if(intervalPeriod < this.minimumPeriod){
+                    node.status({
+                        fill: "yellow",
+                        shape: "dot",
+                        text: "Too Slow"
+                    });
+    
+                    node.send([null, msg, null]);
+                }
+                else{
                     node.status({
                         fill: "green",
                         shape: "dot",
-                        text: "Ok"
+                        text: "Normal"
                     });
-                    msg.timestamp = nowtime;
+    
                     node.send([msg, null, null]);
-                } else if (difference < this.max) {
-                    node.status({
-                        fill: "yellow",
-                        shape: "dot",
-                        text: "Too fast"
-                    });
-                    msg.timestamp = nowtime;
-                    node.send([null, msg, null]);
-                } else if (difference > this.min) {
-                    node.status({
-                        fill: "yellow",
-                        shape: "dot",
-                        text: "Too slow"
-                    });
-                    msg.timestamp = nowtime;
-                    node.send([null, null, msg]);
                 }
-
             }
-            this.lastTimestamp = nowtime;
-        });
+        }
+        );
     }
 
     RED.nodes.registerType("timing", Timing);
