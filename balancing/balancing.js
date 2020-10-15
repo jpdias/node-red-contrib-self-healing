59b0b22ee4c30d23242c346321f0ee9e959978e7
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2020-10-15 15:12:50 +0100

    feat: add dynamic number of outputs

--- balancing/balancing.js
+++ balancing/balancing.js
@@ -1,13 +1,44 @@
 module.exports = function(RED) {
-    function Balancing(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+  function Balancing(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    /**
+     * Algorithm values:
+     *   1: Round Robin
+     *   2: Weighted Round Robin
+     *   3: Random Distribution
+     */
+    this.algorithm = config.algorithm; 
+    this.outputs = config.outputs;
+    
+    
+    this.on("input", function (msg) {
+      this.outputArray = new Array(this.outputs);
+      this.outputArray.fill(null);
+      
+      switch (this.algorithm) {
+        case "1":
+          //Round Robin (TODO)
+          break;
+    
+        case "2":
+          //Weighted Round Robin (TODO)
+          break;
+     
+        case "3":
+          //Random Distribution
+          var out = Math.floor((Math.random() * 4));
+          this.outputArray[out] = msg;
+          
+          node.send(this.outputArray);
+          break;
 
-        this.on("input", function (msg) {
-            node.send([msg, msg]);
-        });
+        default:
+          break;
+      }
+    });
 
-    }
+  }
 
-    RED.nodes.registerType("balancing", Balancing);
+  RED.nodes.registerType("balancing", Balancing);
 }
