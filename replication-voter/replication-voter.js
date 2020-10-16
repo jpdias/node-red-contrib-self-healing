5628feb784c135787fa63901bd0681b6d4d95953
André Gomes <andremsgomes@gmail.com>
2020-10-16 22:33:01 +0100

    style: Format replication-voter code

--- replication-voter/replication-voter.js
+++ replication-voter/replication-voter.js
@@ -1,20 +1,24 @@
-module.exports = function(RED) {
-    const mode = (myArray) =>
-        myArray.reduce(
-            (a, b, i, arr) =>
-            arr.filter((v) => JSON.stringify(v) === JSON.stringify(a)).length >=
-            arr.filter((v) => JSON.stringify(v) === JSON.stringify(b)).length ?
-            a :
-            b,
-            null
-        );
+module.exports = function (RED) {
+  let mode = (myArray) =>
+    myArray.reduce(
+      (a, b, i, arr) =>
+        arr.filter((v) => JSON.stringify(v) === JSON.stringify(a)).length >=
+        arr.filter((v) => JSON.stringify(v) === JSON.stringify(b)).length
+          ? a
+          : b,
+      null
+    );
 
-    function ReplicationVoter(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-        var allValues = [];
+  function ReplicationVoter(config) {
+    RED.nodes.createNode(this, config);
+    let node = this;
+    let allValues = [];
 
+<<<<<<< HEAD
     const sendOut = (node, msg, done, majority) => {
+=======
+    sendOut = (node, msg, done, majority) => {
+>>>>>>> style: Format replication-voter code
       if (majority) {
         node.status({ fill: "green", shape: "dot", text: "Majority" });
         node.send([msg, null]);
@@ -26,39 +30,39 @@ module.exports = function(RED) {
       }
     };
 
-        node.on("input", function(msg, send, done) {
-            if (msg.payload.constructor === Array) {
-                let arrayMode = mode(msg.payload);
-                let majorCount = msg.payload.filter(
-                    (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
-                ).length;
-                msg.payload = arrayMode;
-                sendOut(node, msg, done, majorCount >= parseInt(config.majority));
-            } else if (
-                msg.payload.constructor === Number &&
-                typeof config.countInputs != "undefined"
-            ) {
-                allValues.push(msg.payload);
-                if (allValues.length == config.countInputs) {
-                    let arrayMode = mode(allValues);
-                    let majorCount = allValues.filter(
-                        (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
-                    ).length;
-                    msg.payload = arrayMode;
-                    allValues = [];
-                    sendOut(node, msg, done, majorCount >= parseInt(config.majority));
-                }
-            } else {
-                node.status({
-                    fill: "red",
-                    shape: "dot",
-                    text: "Error: Unexpected Input",
-                });
-                allValues = [];
-                done();
-            }
+    node.on("input", function (msg, send, done) {
+      if (msg.payload.constructor === Array) {
+        let arrayMode = mode(msg.payload);
+        let majorCount = msg.payload.filter(
+          (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
+        ).length;
+        msg.payload = arrayMode;
+        sendOut(node, msg, done, majorCount >= parseInt(config.majority));
+      } else if (
+        msg.payload.constructor === Number &&
+        typeof config.countInputs != "undefined"
+      ) {
+        allValues.push(msg.payload);
+        if (allValues.length == config.countInputs) {
+          let arrayMode = mode(allValues);
+          let majorCount = allValues.filter(
+            (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
+          ).length;
+          msg.payload = arrayMode;
+          allValues = [];
+          sendOut(node, msg, done, majorCount >= parseInt(config.majority));
+        }
+      } else {
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: "Error: Unexpected Input",
         });
-    }
+        allValues = [];
+        done();
+      }
+    });
+  }
 
-    RED.nodes.registerType("replication-voter", ReplicationVoter);
+  RED.nodes.registerType("replication-voter", ReplicationVoter);
 };
