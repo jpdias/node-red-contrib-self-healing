54b73107d3e5aa97174f41ac42ca4dc392427552
Miguel Pinto <pt.miguel99@gmail.com>
2020-10-18 00:13:10 +0100

    test: Add remaining tests

--- test/threshold-check_spec.js
+++ test/threshold-check_spec.js
@@ -118,7 +118,6 @@ describe("threshold-check node", function () {
   /**
    * Equal rule
    */
-
   it("should fail on equal operation that is different", function (done) {
     let equalRuleParams = {
       type: "eq",
@@ -148,7 +147,6 @@ describe("threshold-check node", function () {
   /**
    * Not equal rule
    */
-
   it("should fail on not equal operation that is equal", function (done) {
     let equalRuleParams = {
       type: "neq",
@@ -178,7 +176,6 @@ describe("threshold-check node", function () {
   /**
    * Less rule
    */
-
   it("should fail on less operation that is greater", function (done) {
     let lessRuleParams = {
       type: "lt",
@@ -221,7 +218,6 @@ describe("threshold-check node", function () {
   /**
    * Less or equal rule
    */
-
   it("should fail on less or qual operation that is greater", function (done) {
     let lessOrEqualRuleParams = {
       type: "lte",
@@ -264,7 +260,6 @@ describe("threshold-check node", function () {
   /**
    * Greater rule
    */
-
   it("should fail on greater operation that is less", function (done) {
     let greaterRuleParams = {
       type: "gt",
@@ -349,7 +344,6 @@ describe("threshold-check node", function () {
   /**
    * True rule
    */
-
   it("should fail on true operation that is false", function (done) {
     let trueRuleParams = {
       type: "true",
@@ -402,10 +396,10 @@ describe("threshold-check node", function () {
     basicRuleTest(trueRuleParams, true, null, done);
   });
 
+  
   /**
    * False rule
    */
-
   it("should fail on false operation that is true", function (done) {
     let falseRuleParams = {
       type: "false",
@@ -443,5 +437,300 @@ describe("threshold-check node", function () {
     };
 
     basicRuleTest(falseRuleParams, false, false, done);
+  });
+
+  /**
+   * Null rule
+   */
+  it("should fail on null operation that is a string", function (done) {
+    let nullRuleParams = {
+      type: "null",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(nullRuleParams, true, "wabba lubba dub dub", done);
+  });
+
+  it("should fail on null operation that is a number", function (done) {
+    let nullRuleParams = {
+      type: "null",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(nullRuleParams, true, 430, done);
+  });
+
+  it("should pass on null operation that is null", function (done) {
+    let nullRuleParams = {
+      type: "null",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(nullRuleParams, false, null, done);
+  });
+
+  /**
+   * Not null rule
+   */
+  it("should pass on not null operation that is a string", function (done) {
+    let notNullRuleParams = {
+      type: "nnull",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(notNullRuleParams, false, "wabba lubba dub dub", done);
+  });
+
+  it("should pass on not null operation that is a number", function (done) {
+    let notNullRuleParams = {
+      type: "nnull",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(notNullRuleParams, false, 430, done);
+  });
+
+  it("should fail on not null operation that is null", function (done) {
+    let notNullRuleParams = {
+      type: "nnull",
+      value: "",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(notNullRuleParams, true, null, done);
+  });
+
+  /**
+   * Between rule
+   */
+  it("should pass on between rule", function (done) {
+    let betweenRuleParams = {
+      type: "btwn",
+      value: "10",
+      valueType: "num",
+      value2: "20",
+      valueType2: "num",
+      case: false,
+    };
+
+    basicRuleTest(betweenRuleParams, false, 15, done);
+  });
+
+  it("should fail on between rule", function (done) {
+    let betweenRuleParams = {
+      type: "btwn",
+      value: "10",
+      valueType: "num",
+      value2: "20",
+      valueType2: "num",
+      case: false,
+    };
+
+    basicRuleTest(betweenRuleParams, true, 50, done);
+  });
+
+  /**
+   * Regex rule
+   */
+  it("should pass on regex rule with a string", function (done) {
+    let regexRuleParams = {
+      type: "regex",
+      value: "b[aeiou]t",
+      valueType: "re",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(regexRuleParams, false, "cricket bat", done);
+  });
+
+  /**
+   * Contains rule
+   */
+  it("should pass on contains rule with array of integers with a given number", function (done) {
+    let containsRuleParams = {
+      type: "cont",
+      value: "2",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(containsRuleParams, false, "[1,2,3]", done);
+  });
+
+  it("should fail on contains rule with array of integers without a given number", function (done) {
+    let containsRuleParams = {
+      type: "cont",
+      value: "4",
+      valueType: "str",
+      value2: "",
+      valueType2: "str",
+      case: false,
+    };
+
+    basicRuleTest(containsRuleParams, true, "[1,2,3]", done);
+  });
+
+  /**
+   * Within rule
+   */
+  it("should pass on within rule with mean and stable values", function (done) {
+    let rule = {
+      property: "payload",
+      propertyType: "msg",
+      type: "within",
+      value: "2",
+      valueType: "num",
+      value2: "3",
+      valueType2: "mean",
+      case: false,
+      failMsg: ""
+    };
+
+    let testFlow = [
+      {
+        id: "n1",
+        type: "threshold-check",
+        name: "threshold-check",
+        rules: [rule],
+        wires: [["n2"], ["n3"]],
+      },
+      {
+        id: "n2",
+        type: "helper",
+        name: "success-output-from-test-node",
+      },
+      {
+        id: "n3",
+        type: "helper",
+        name: "error-output-from-test-node",
+      },
+    ];
+
+    helper.load(thresholdCheckNode, testFlow, function () {
+      let error = {
+        exist: false,
+        description: "",
+      };
+
+      let errorNode = helper.getNode("n3");
+      errorNode.on("input", (msg) => {
+        try {
+          msg.should.have.property("fault");
+          error.exist = true;
+          error.description = "Error node received input when it shouldn't";
+        } catch (err) {
+          error.exist = true;
+          error.description = "Message has no fault parameter when it should";
+        }
+      });
+
+      let testNode = helper.getNode("n1");
+      testNode.receive({ payload: 6});
+      testNode.receive({ payload: 8});
+      testNode.receive({ payload: 7});
+
+      setTimeout(() => {
+        if (error.exist) {
+          done(error.description);
+        } else {
+          done();
+        }
+      }, 200);
+    });
+  });
+
+  it("should fail on within rule with mean and alien value", function (done) {
+    let rule = {
+      property: "payload",
+      propertyType: "msg",
+      type: "within",
+      value: "10",
+      valueType: "num",
+      value2: "5",
+      valueType2: "mean",
+      case: false,
+      failMsg: ""
+    };
+
+    let testFlow = [
+      {
+        id: "n1",
+        type: "threshold-check",
+        name: "threshold-check",
+        rules: [rule],
+        wires: [["n2"], ["n3"]],
+      },
+      {
+        id: "n2",
+        type: "helper",
+        name: "success-output-from-test-node",
+      },
+      {
+        id: "n3",
+        type: "helper",
+        name: "error-output-from-test-node",
+      },
+    ];
+
+    helper.load(thresholdCheckNode, testFlow, function () {
+      let error = {
+        exist: true,
+        description: "",
+      };
+
+      let errorNode = helper.getNode("n3");
+      errorNode.on("input", (msg) => {
+        try {
+          msg.should.have.property("fault");
+          error.exist = false;
+        } catch (err) {
+          error.exist = true;
+          error.description = "Message has no fault parameter when it should";
+        }
+      });
+
+      let testNode = helper.getNode("n1");
+      testNode.receive({ payload: 10});
+      testNode.receive({ payload: 12});
+      testNode.receive({ payload: 15});
+      testNode.receive({ payload: 11});
+      testNode.receive({ payload: 35});
+      testNode.receive({ payload: 14});
+
+      setTimeout(() => {
+        if (error.exist) {
+          done(error.description);
+        } else {
+          done();
+        }
+      }, 200);
+    });
   });
 });
