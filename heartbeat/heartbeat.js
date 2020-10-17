60ab1432ab93d6d13cc898c67c21b1fc3fd88879
Nokas <up201605335@fe.up.pt>
2020-10-17 22:14:00 +0100

    feat: reimplemented http connection heartbeat

--- heartbeat/heartbeat.js
+++ heartbeat/heartbeat.js
@@ -1,92 +1,59 @@
 module.exports = function (RED) {
   const https = require("https");
 
-  function failedTrigger(node) {
-    node.send({
-      payload: { status: 0, statusMessage: "Timeout" },
-      timestamp: Date.now().toString(),
-    });
-  }
-
   function Heartbeat(config) {
     RED.nodes.createNode(this, config);
 
-    this.interval_id = null;
-    this.repeat = config.frequency;
-    this.httpendpoint = config.httpendpoint;
+    this.interval = null;
+    this.frequency = config.frequency;
     this.protocol = config.protocol;
     this.onfail = config.onfail;
-    var node = this;
-
-    node.repeaterSetup = function () {
-      this.interval_id = setInterval(function () {
-        node.emit("input", {});
-      }, this.repeat * 1000); //from milis to seconds
-    };
+    this.httpendpoint = config.httpendpoint;
+    let node = this;
 
     if (this.protocol == "http") {
-      node.repeaterSetup();
+      this.interval = setInterval(function () {
+        node.emit("input", {});
+      }, this.frequency * 1000); //from seconds to milliseconds
     }
 
-    this.on("input", function () {
+    this.on("input", function (msg, send, done) {
+      //HTTP protocol
       if (this.protocol == "http") {
         https
-          .get(this.httpendpoint, (resp) => {
-            const { statusCode, statusMessage } = resp;
+          .get(this.httpendpoint, (res) => {
+            const { statusCode, statusMessage } = res;
+            //everything OK
             if (statusCode == 200) {
-              node.status({
-                fill: "green",
-                shape: "dot",
-                text: "Ok",
-              });
+              this.status({ fill: "green", shape: "dot", text: "OK" });
               if (!this.onfail) {
-                node.send({
-                  payload: { status: statusCode, statusMessage: statusMessage },
-                  timestamp: Date.now().toString(),
-                });
+                msg.payload = {
+                  status: statusCode,
+                  statusMessage: statusMessage,
+                };
+                msg.timestamp = Date.now().toString();
+                send(msg);
               }
             } else {
-              node.status({
-                fill: "red",
-                shape: "circle",
-                text: "Failed",
-              });
-              node.send({
-                payload: { status: statusCode, statusMessage: statusMessage },
-                timestamp: Date.now().toString(),
-              });
+              this.status({ fill: "red", shape: "ring", text: "FAIL" });
+              msg.payload = {
+                status: statusCode,
+                statusMessage: statusMessage,
+              };
+              msg.timestamp = Date.now().toString();
+              send(msg);
             }
           })
           .on("error", (err) => {
-            node.status({
-              fill: "red",
-              shape: "dot",
-              text: "Error",
-            });
-            node.send({
-              payload: { status: 0, statusMessage: err.message },
-              timestamp: Date.now().toString(),
-            });
+            this.status({ fill: "red", shape: "dot", text: "ERROR" });
+            msg.payload = { status: 0, statusMessage: err.message };
+            msg.timestamp = Date.now().toString();
+            send(msg);
           });
-      } else if (this.protocol == "mqtt") {
-        if (!this.interval_id) {
-          this.interval_id = setInterval(
-            failedTrigger,
-            parseInt(this.repeat) * 1000,
-            node
-          );
-        }
-        clearInterval(this.interval_id);
-
-        if (!this.onfail) {
-          node.send({ payload: { status: 200, statusMessage: "Alive" } });
-        }
-
-        this.interval_id = setInterval(
-          failedTrigger,
-          parseInt(this.repeat) * 1000,
-          node
-        );
+      }
+      //MQTT protocol
+      else if (this.protocol == "mqtt") {
+        //do something
       }
     });
   }
