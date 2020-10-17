054ff971b774d1d6f431d76744839536b0748632
André Gomes <andremsgomes@gmail.com>
2020-10-17 23:58:44 +0100

    refactor: split methods into smaller ones

--- replication-voter/replication-voter.js
+++ replication-voter/replication-voter.js
@@ -1,5 +1,5 @@
 module.exports = function (RED) {
-  let mode = (myArray) =>
+  function mode(myArray) {
     myArray.reduce(
       (a, b, i, arr) =>
         arr.filter((v) => JSON.stringify(v) === JSON.stringify(a)).length >=
@@ -8,58 +8,88 @@ module.exports = function (RED) {
           : b,
       null
     );
+  }
+
+  function setMajorityStatus(node, msg, done) {
+    node.status({
+      fill: "green",
+      shape: "dot",
+      text: "Majority",
+    });
+
+    node.send([msg, null]);
+
+    done();
+  }
+
+  function setNoMajorityStatus(node, msg, done) {
+    node.status({
+      fill: "yellow",
+      shape: "dot",
+      text: "No Majority",
+    });
+
+    node.send([null, msg]);
+
+    done();
+  }
+
+  function setErrorStatus(node, done) {
+    node.status({
+      fill: "red",
+      shape: "dot",
+      text: "Error: Unexpected Input",
+    });
+
+    done();
+  }
+
+  function sendOut(node, msg, done, majority) {
+    if (majority) {
+      setMajorityStatus(node, msg, done);
+    } else {
+      setNoMajorityStatus(node, msg, done);
+    }
+  }
 
   function ReplicationVoter(config) {
     RED.nodes.createNode(this, config);
     let node = this;
     let allValues = [];
 
-<<<<<<< HEAD
-    const sendOut = (node, msg, done, majority) => {
-=======
-    sendOut = (node, msg, done, majority) => {
->>>>>>> style: Format replication-voter code
-      if (majority) {
-        node.status({ fill: "green", shape: "dot", text: "Majority" });
-        node.send([msg, null]);
-        done();
-      } else {
-        node.status({ fill: "yellow", shape: "dot", text: "No majority" });
-        node.send([null, msg]);
-        done();
-      }
-    };
-
     node.on("input", function (msg, send, done) {
       if (msg.payload.constructor === Array) {
         let arrayMode = mode(msg.payload);
+
         let majorCount = msg.payload.filter(
           (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
         ).length;
+
         msg.payload = arrayMode;
+
         sendOut(node, msg, done, majorCount >= parseInt(config.majority));
       } else if (
         msg.payload.constructor === Number &&
         typeof config.countInputs != "undefined"
       ) {
         allValues.push(msg.payload);
+
         if (allValues.length == config.countInputs) {
           let arrayMode = mode(allValues);
+
           let majorCount = allValues.filter(
             (x) => JSON.stringify(x) == JSON.stringify(arrayMode)
           ).length;
+
           msg.payload = arrayMode;
           allValues = [];
+
           sendOut(node, msg, done, majorCount >= parseInt(config.majority));
         }
       } else {
-        node.status({
-          fill: "red",
-          shape: "dot",
-          text: "Error: Unexpected Input",
-        });
         allValues = [];
-        done();
+
+        setErrorStatus(node, done);
       }
     });
   }
