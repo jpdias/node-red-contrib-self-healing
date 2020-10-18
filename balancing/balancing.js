29faacf56b89af8f2a07b92f1b5abdcd86496620
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2020-10-18 16:21:04 +0100

    feat: Add Weighted Round Robin algorithm

--- balancing/balancing.js
+++ balancing/balancing.js
@@ -2,7 +2,10 @@ module.exports = function(RED) {
   function Balancing(config) {
     RED.nodes.createNode(this, config);
     let node = this;
+
+    // Number of outputs for the node
     let numberOutputs = config.outputs;
+    
     /**
      * Algorithm values:
      *   1: Round Robin
@@ -11,32 +14,56 @@ module.exports = function(RED) {
      */
     let algorithm = config.algorithm;
     
-    let lastOutputUsed = 0;
+    /** 
+     * Array with weights for the outputs.
+     * Only used when Weighted Round Robin algorithm is chosen
+     */ 
+    let wei = config.weights.split(".").map(Number);
+
+    let roundRobinOutput = 0;
 
     this.on("input", function (msg) {
+      // Create the array that is used to output the message
       let outputArray = new Array(numberOutputs);
       outputArray.fill(null);
-      
-      switch (algorithm) {
-        case "1":
-          //Round Robin
-          outputArray[lastOutputUsed] = msg;
 
-          lastOutputUsed++;
-          if (lastOutputUsed == numberOutputs) {
-              lastOutputUsed = 0;
+      let out;
+
+      switch (algorithm) {
+        case "1":   //Round Robin
+          outputArray[roundRobinOutput] = msg;
+
+          roundRobinOutput++;
+          if (roundRobinOutput == numberOutputs) {
+            roundRobinOutput = 0;
           }
 
           node.send(outputArray);
           break;
     
-        case "2":
-          //Weighted Round Robin (TODO)
+        case "2":   //Weighted Round Robin 
+
+          let totalWeight = wei.reduce((x,y) => x+y);
+          
+          let rand = Math.random();
+          let a = 0;
+          out = 0;
+          for (let i = 0; i < wei.length; i++) {
+            if (a <= rand && rand < (wei[i]/totalWeight)+a) {
+              out = i;
+              break;
+            } else {
+              a = (wei[i]/totalWeight) + a;
+            }
+            
+          }
+          
+          outputArray[out] = msg;
+          node.send(outputArray);
           break;
      
-        case "3":
-          //Random Distribution
-          let out = Math.floor((Math.random() * 4));
+        case "3":   //Random Distribution
+          out = Math.floor((Math.random() * numberOutputs));
           outputArray[out] = msg;
           
           node.send(outputArray);
