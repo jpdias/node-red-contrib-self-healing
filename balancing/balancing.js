e5b02562a1917a5784837db5b3557afa4037b2e7
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2020-10-19 12:02:08 +0100

    style: format code with prettier

--- balancing/balancing.js
+++ balancing/balancing.js
@@ -1,11 +1,11 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
   function Balancing(config) {
     RED.nodes.createNode(this, config);
     let node = this;
 
     // Number of outputs for the node
     let numberOutputs = config.outputs;
-    
+
     /**
      * Algorithm values:
      *   1: Round Robin
@@ -13,11 +13,11 @@ module.exports = function(RED) {
      *   3: Random Distribution
      */
     let algorithm = config.algorithm;
-    
-    /** 
+
+    /**
      * Array with weights for the outputs.
      * Only used when Weighted Round Robin algorithm is chosen
-     */ 
+     */
     let wei = config.weights.split(".").map(Number);
 
     let roundRobinOutput = 0;
@@ -30,7 +30,8 @@ module.exports = function(RED) {
       let out;
 
       switch (algorithm) {
-        case "1": {  //Round Robin
+        case "1": {
+          //Round Robin
           outputArray[roundRobinOutput] = msg;
 
           roundRobinOutput++;
@@ -41,32 +42,33 @@ module.exports = function(RED) {
           node.send(outputArray);
           break;
         }
-        case "2": { //Weighted Round Robin 
+        case "2": {
+          //Weighted Round Robin
 
-          let totalWeight = wei.reduce((x,y) => x+y);
-          
+          let totalWeight = wei.reduce((x, y) => x + y);
+
           let rand = Math.random();
           let a = 0;
           out = 0;
           for (let i = 0; i < wei.length; i++) {
-            if (a <= rand && rand < (wei[i]/totalWeight)+a) {
+            if (a <= rand && rand < wei[i] / totalWeight + a) {
               out = i;
               break;
             } else {
-              a = (wei[i]/totalWeight) + a;
+              a = wei[i] / totalWeight + a;
             }
-            
           }
-          
+
           outputArray[out] = msg;
           node.send(outputArray);
           break;
         }
 
-        case "3": {  //Random Distribution
-          out = Math.floor((Math.random() * numberOutputs));
+        case "3": {
+          //Random Distribution
+          out = Math.floor(Math.random() * numberOutputs);
           outputArray[out] = msg;
-          
+
           node.send(outputArray);
           break;
         }
@@ -76,8 +78,7 @@ module.exports = function(RED) {
         }
       }
     });
-
   }
 
   RED.nodes.registerType("balancing", Balancing);
-}
+};
--- test/balancing_spec.js
+++ test/balancing_spec.js
@@ -4,8 +4,7 @@ let balancingNode = require("../balancing/balancing.js");
 
 helper.init(require.resolve("node-red"));
 
-describe('balancing node', function () {
-    
+describe("balancing node", function () {
   beforeEach(function (done) {
     helper.startServer(done);
   });
@@ -15,8 +14,17 @@ describe('balancing node', function () {
     helper.stopServer(done);
   });
 
-  it('should be loaded', function (done) {
-    let flow = [{id: "n1", type: "balancing", name: "balancing", outputs: 3, algorithm: "1", weights: "1.1.1"}];
+  it("should be loaded", function (done) {
+    let flow = [
+      {
+        id: "n1",
+        type: "balancing",
+        name: "balancing",
+        outputs: 3,
+        algorithm: "1",
+        weights: "1.1.1",
+      },
+    ];
 
     helper.load(balancingNode, flow, function () {
       let n1 = helper.getNode("n1");
@@ -25,16 +33,24 @@ describe('balancing node', function () {
         done();
       } catch (error) {
         done(error);
-      }     
+      }
     });
   });
 
   function testNode(algorithm, done) {
     let flow = [
-      {id: "n1", type: "balancing", name: "balancing", outputs: 3, algorithm: algorithm, weights: "1.1.1", wires: [["n2"],["n3"],["n4"]]},
-      {id: "n2", type: "helper"},
-      {id: "n3", type: "helper"},
-      {id: "n4", type: "helper"}
+      {
+        id: "n1",
+        type: "balancing",
+        name: "balancing",
+        outputs: 3,
+        algorithm: algorithm,
+        weights: "1.1.1",
+        wires: [["n2"], ["n3"], ["n4"]],
+      },
+      { id: "n2", type: "helper" },
+      { id: "n3", type: "helper" },
+      { id: "n4", type: "helper" },
     ];
 
     helper.load(balancingNode, flow, function () {
@@ -46,41 +62,37 @@ describe('balancing node', function () {
       let count = 0;
 
       n2.on("input", function (msg, _send, _done) {
-        should(msg).have.property('payload', 'Testing');
+        should(msg).have.property("payload", "Testing");
         count++;
       });
 
       n3.on("input", function (msg, _send, _done) {
-        should(msg).have.property('payload', 'Testing');
+        should(msg).have.property("payload", "Testing");
         count++;
       });
 
       n4.on("input", function (msg, _send, _done) {
-        should(msg).have.property('payload', 'Testing');
+        should(msg).have.property("payload", "Testing");
         count++;
       });
 
-      n1.receive({payload: "Testing"});
+      n1.receive({ payload: "Testing" });
 
       setTimeout(function () {
-        if(count == 1)
-          done();
+        if (count == 1) done();
       }, 1000);
     });
   }
 
-
-  it('should send message to only one output using Round Robin algorithm', function (done) {
+  it("should send message to only one output using Round Robin algorithm", function (done) {
     testNode("1", done);
   });
-    
-  it('should send message to only one output using Weighted Round Robin algorithm', function (done) {
+
+  it("should send message to only one output using Weighted Round Robin algorithm", function (done) {
     testNode("2", done);
   });
-    
-  it('should send message to only one output using Random algorithm', function (done) {
+
+  it("should send message to only one output using Random algorithm", function (done) {
     testNode("3", done);
   });
-
-    
 });
