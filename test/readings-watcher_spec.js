1dd30e2984a0299e3e0079fd64b0ff889074bd6f
Nelson Gregorio <nelson.gregorio@live.com.pt>
2020-10-19 21:32:26 +0100

    style: Format code

--- test/readings-watcher_spec.js
+++ test/readings-watcher_spec.js
@@ -5,251 +5,249 @@ helper.init(require.resolve("node-red"));
 
 let testflow = [];
 
-describe('readings-watcher node', function () {
+describe("readings-watcher node", function () {
+  beforeEach(function (done) {
+    testflow = [
+      {
+        id: "watcher",
+        type: "readings-watcher",
+        name: "readings-watcher",
+        wires: [["ok"], ["error"]],
+      },
+      { id: "ok", type: "helper" },
+      { id: "error", type: "helper" },
+    ];
+    helper.startServer(done);
+  });
 
-	beforeEach(function (done) {
-		testflow =
-		[
-			{ "id": "watcher", "type": "readings-watcher", "name": "readings-watcher", "wires": [["ok"], ["error"]] },
-			{ "id": "ok", "type": "helper" },
-			{ "id": "error", "type": "helper" },
-		];
-		helper.startServer(done);
-	});
+  afterEach(function (done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
 
-	afterEach(function (done) {
-		helper.unload();
-		helper.stopServer(done);
-	});
+  it("should be loaded", function (done) {
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      try {
+        watcher.should.have.property("name", "readings-watcher");
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
 
-	it('should be loaded', function (done) {
-		let flow = [ {id: "watcher", type: "readings-watcher", name: "readings-watcher" }];
+  it("should report if input is not a number", function (done) {
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
 
-		helper.load(readingswatcher, flow, function () {
-			let watcher = helper.getNode("watcher");
-			try {
-				watcher.should.have.property("name", "readings-watcher");
-				done();
-			} catch (error) {
-				done(error);
-			}
-		});
-	});
+      watcher.receive({ payload: "NaN" });
+      setTimeout(() => {
+        try {
+          watcher.warn.should.be.calledOnce();
+          done();
+        } catch (error) {
+          done(error);
+        }
+      }, 10);
+    });
+  });
 
-	it('should report if input is not a number', function (done) {
+  it("should allow consecutive readings when using 'minchange' and their values are sufficiently different", function (done) {
+    let messageSequence = [1, 1.5];
+    let minchange = 0.1;
+    minchange.should.be.lessThan(
+      Math.abs(messageSequence[0] - messageSequence[1]) / messageSequence[0]
+    );
+    testflow[0].strategyMask = 1;
+    testflow[0].minchange = minchange;
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let receivedMsgs = 0;
 
-			watcher.receive({payload: "NaN"});
-			setTimeout(() => {
-				try {
-					watcher.warn.should.be.calledOnce();
-					done();
-				} catch (error) {
-					done(error);
-				}
-			}, 10);
-		});
-	});
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          if (receivedMsgs == messageSequence.length) done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-	it('should allow consecutive readings when using \'minchange\' and their values are sufficiently different', function (done) {
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 
-		let messageSequence = [1, 1.5];
-		let minchange = 0.1;
-		minchange.should.be.lessThan(Math.abs(messageSequence[0]-messageSequence[1])/messageSequence[0]);
-		testflow[0].strategyMask = 1;
-		testflow[0].minchange = minchange;
+  it("should allow consecutive readings when using 'maxchange' and their values are sufficiently similar", function (done) {
+    let messageSequence = [0.9, 1.1];
+    let maxchange = 0.5;
+    maxchange.should.be.greaterThan(
+      Math.abs(messageSequence[0] - messageSequence[1]) / messageSequence[0]
+    );
+    testflow[0].strategyMask = 2;
+    testflow[0].maxchange = maxchange;
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let receivedMsgs = 0;
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let receivedMsgs = 0;
 
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					if(receivedMsgs == messageSequence.length)
-						done();
-				} catch (err) {
-				  done(err);
-				}
-			});
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          if (receivedMsgs == messageSequence.length) done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 
-	it('should allow consecutive readings when using \'maxchange\' and their values are sufficiently similar', function (done) {
+  it("should allow consecutive readings when using 'stucklimit' and the last 'X' values are not all exactly equal", function (done) {
+    let messageSequence = [1, 1, 1, 2, 1, 1];
+    let stucklimit = 4;
+    testflow[0].strategyMask = 4;
+    testflow[0].stucklimit = stucklimit;
 
-		let messageSequence = [0.9, 1.1];
-		let maxchange = 0.5;
-		maxchange.should.be.greaterThan(Math.abs(messageSequence[0]-messageSequence[1])/messageSequence[0]);
-		testflow[0].strategyMask = 2;
-		testflow[0].maxchange = maxchange;
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let receivedMsgs = 0;
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let receivedMsgs = 0;
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          if (receivedMsgs == messageSequence.length) done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					if(receivedMsgs == messageSequence.length)
-						done();
-				} catch (err) {
-				  done(err);
-				}
-			});
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
+  it("should trigger an error when using 'minchange' and consecutive readings are too similar", function (done) {
+    let messageSequence = [1, 1.05];
+    let minchange = 0.1;
+    minchange.should.be.greaterThanOrEqual(
+      Math.abs(messageSequence[0] - messageSequence[1]) / messageSequence[0]
+    );
+    testflow[0].strategyMask = 1;
+    testflow[0].minchange = minchange;
 
-	it('should allow consecutive readings when using \'stucklimit\' and the last \'X\' values are not all exactly equal', function (done) {
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let error = helper.getNode("error");
+      let receivedMsgs = 0;
 
-		let messageSequence = [1, 1, 1, 2, 1, 1];
-		let stucklimit = 4;
-		testflow[0].strategyMask = 4;
-		testflow[0].stucklimit = stucklimit;
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+        } catch (err) {
+          done(err);
+        }
+      });
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let receivedMsgs = 0;
+      error.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          receivedMsgs.should.be.equal(messageSequence.length);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					if(receivedMsgs == messageSequence.length)
-						done();
-				} catch (err) {
-				  done(err);
-				}
-			});
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
+  it("should trigger an error when using 'maxchange' and consecutive readings are too different", function (done) {
+    let messageSequence = [0.3, 0.5];
+    let maxchange = 0.65;
+    maxchange.should.be.lessThanOrEqual(
+      Math.abs(messageSequence[0] - messageSequence[1]) / messageSequence[0]
+    );
+    testflow[0].strategyMask = 2;
+    testflow[0].maxchange = maxchange;
 
-	it('should trigger an error when using \'minchange\' and consecutive readings are too similar', function (done) {
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let error = helper.getNode("error");
+      let receivedMsgs = 0;
 
-		let messageSequence = [1, 1.05];
-		let minchange = 0.1;
-		minchange.should.be.greaterThanOrEqual(Math.abs(messageSequence[0]-messageSequence[1])/messageSequence[0]);
-		testflow[0].strategyMask = 1;
-		testflow[0].minchange = minchange;
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+        } catch (err) {
+          done(err);
+        }
+      });
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let error = helper.getNode("error");
-			let receivedMsgs = 0;
+      error.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          receivedMsgs.should.be.equal(messageSequence.length);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-				} catch (err) {
-				  done(err);
-				}
-			});
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 
-			error.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					receivedMsgs.should.be.equal(messageSequence.length);
-					done();
-				} catch (err) {
-				  done(err);
-				}
-			});
+  it("should trigger an error when using 'stucklimit' and the last 'X' consecutive readings are equal", function (done) {
+    let messageSequence = [1, 1, 1, 1, 1];
+    let stucklimit = messageSequence.length;
+    testflow[0].strategyMask = 4;
+    testflow[0].stucklimit = stucklimit;
 
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
+    helper.load(readingswatcher, testflow, function () {
+      let watcher = helper.getNode("watcher");
+      let ok = helper.getNode("ok");
+      let error = helper.getNode("error");
+      let receivedMsgs = 0;
 
-	it('should trigger an error when using \'maxchange\' and consecutive readings are too different', function (done) {
+      ok.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+        } catch (err) {
+          done(err);
+        }
+      });
 
-		let messageSequence = [0.3, 0.5];
-		let maxchange = 0.65;
-		maxchange.should.be.lessThanOrEqual(Math.abs(messageSequence[0]-messageSequence[1])/messageSequence[0]);
-		testflow[0].strategyMask = 2;
-		testflow[0].maxchange = maxchange;
+      error.on("input", function (msg) {
+        try {
+          msg.payload.should.equal(messageSequence[receivedMsgs++]);
+          receivedMsgs.should.be.equal(stucklimit);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
 
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let error = helper.getNode("error");
-			let receivedMsgs = 0;
-
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-				} catch (err) {
-				  done(err);
-				}
-			});
-
-			error.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					receivedMsgs.should.be.equal(messageSequence.length);
-					done();
-				} catch (err) {
-				  done(err);
-				}
-			});
-
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
-
-	it('should trigger an error when using \'stucklimit\' and the last \'X\' consecutive readings are equal', function (done) {
-
-		let messageSequence = [1, 1, 1, 1, 1];
-		let stucklimit = messageSequence.length;
-		testflow[0].strategyMask = 4;
-		testflow[0].stucklimit = stucklimit;
-
-		helper.load(readingswatcher, testflow, function () {
-			let watcher = helper.getNode("watcher");
-			let ok = helper.getNode("ok");
-			let error = helper.getNode("error");
-			let receivedMsgs = 0;
-
-			ok.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-				} catch (err) {
-				  done(err);
-				}
-			});
-
-			error.on("input", function (msg) {
-				try {
-					msg.payload.should.equal(messageSequence[receivedMsgs++]);
-					receivedMsgs.should.be.equal(stucklimit);
-					done();
-				} catch (err) {
-				  done(err);
-				}
-			});
-
-			messageSequence.forEach(element => {
-				watcher.receive({ payload: element });
-			});
-		});
-	});
-
+      messageSequence.forEach((element) => {
+        watcher.receive({ payload: element });
+      });
+    });
+  });
 });
