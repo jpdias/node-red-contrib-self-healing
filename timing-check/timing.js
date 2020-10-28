3fad958cc7b617c4979272009cc779e97d58e0bc
SALajes <sofialajes98@gmail.com>
2020-10-28 10:44:24 +0000

    docs: added Timeout and SlidingWindowLength input fields and documentation in the html

--- timing-check/timing.js
+++ timing-check/timing.js
@@ -1,3 +1,6 @@
+// Instead of measuring flow every two consecutive messages, measure the average flow of X consecutive messages (moving average)
+// Implement output for timeout
+
 module.exports = function (RED) {
   function Timing(config) {
     RED.nodes.createNode(this, config);
@@ -5,6 +8,9 @@ module.exports = function (RED) {
     this.lastTimestamp = null;
     this.periodBetweenReadings = config.period * 1000; //seconds to milliseconds
     this.intervalMargin = config.margin;
+    this.slidingWindow = [];
+    this.slidingWindowLength = config.slidingWindow;
+    this.timeout = config.timeout * 1000;
 
     this.maximumPeriod =
       this.periodBetweenReadings +
@@ -26,7 +32,7 @@ module.exports = function (RED) {
           text: "First message",
         });
 
-        node.send([msg, null, null]);
+        node.send([msg, null, null, null]);
       } else {
         const intervalPeriod = currentTimestamp - this.lastTimestamp;
 
@@ -37,7 +43,7 @@ module.exports = function (RED) {
             text: "Too Slow",
           });
 
-          node.send([null, null, msg]);
+          node.send([null, null, msg, null]);
         } else if (intervalPeriod < this.minimumPeriod) {
           node.status({
             fill: "yellow",
@@ -45,7 +51,7 @@ module.exports = function (RED) {
             text: "Too Fast",
           });
 
-          node.send([null, msg, null]);
+          node.send([null, msg, null, null]);
         } else {
           node.status({
             fill: "green",
@@ -53,11 +59,26 @@ module.exports = function (RED) {
             text: "Normal",
           });
 
-          node.send([msg, null, null]);
+          node.send([msg, null, null, null]);
         }
       }
       this.lastTimestamp = currentTimestamp;
     });
+
+    setTimeout(()=>{
+      const currentTimestamp = Date.now();
+      msg.timestamp = currentTimestamp;
+
+      if(currentTimestamp - this.lastTimestamp > this.timeout){
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: "Timeout",
+        });
+
+        node.send([null, null, null, msg]);
+      }
+    }, this.timeout);
   }
 
   RED.nodes.registerType("timing", Timing);
