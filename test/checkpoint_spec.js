a19efe8bf678873aa3cbf70799a8151fbfd4b9d9
Miguel Pinto <pt.miguel99@gmail.com>
2020-11-02 21:33:37 +0000

    test(checkpoint): Add base loading test

---
+++ test/checkpoint_spec.js
@@ -0,0 +1,37 @@
+let should = require("should");
+let helper = require("node-red-node-test-helper");
+let checkpointNode = require("../checkpoint/checkpoint.js");
+
+helper.init(require.resolve("node-red"));
+
+describe("checkpoint node", function () {
+  beforeEach(function (done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function (done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  it("should be loaded", function (done) {
+    let flow = [
+      {
+        id: "n1",
+        type: "checkpoint",
+        name: "checkpoint",
+      },
+    ];
+
+    helper.load(checkpointNode, flow, function () {
+      let n1 = helper.getNode("n1");
+      try {
+        n1.should.have.property("name", "checkpoint");
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
+
+});
