5f92339a58193b6b4bfdd671cfb1b114f8e237a9
Miguel Pinto <pt.miguel99@gmail.com>
2020-11-02 19:07:25 +0000

    style(checkpoint): Run format

--- checkpoint/checkpoint.js
+++ checkpoint/checkpoint.js
@@ -17,45 +17,44 @@
  */
 
 module.exports = function (RED) {
-    function checkpoint(config) {
-        RED.nodes.createNode(this, config);
-        let node = this;
+  function checkpoint(config) {
+    RED.nodes.createNode(this, config);
+    let node = this;
 
-        function getPersistentContext(property) {
-            return node.context().get(property, "file");
-        }
-
-        function setPersistentContext(property, value) {
-            return node.context().set(property, value, "file");
-        }
-
-        let active = getPersistentContext("active");
-        let lastMsg = getPersistentContext("lastMsg");
-
-        if (active === undefined) {
-            setPersistentContext("active", false);
-            setPersistentContext("lastMsg", "");
-        } 
-        else if(active === true && lastMsg != undefined) {
-            setTimeout(() => {
-                node.emit("restart", lastMsg);
-            }, 500);
-        }
-
-        node.on("input", function (msg, send, done) {
-            if (getPersistentContext("active") === false)  {
-                setPersistentContext("active", true);
-            }
-            setPersistentContext("lastMsg", msg);
-
-            send([msg]);
-            done();
-        });
-
-        node.on("restart", function (msg) {
-            node.send([msg]);
-        });
+    function getPersistentContext(property) {
+      return node.context().get(property, "file");
     }
 
-    RED.nodes.registerType("checkpoint", checkpoint);
+    function setPersistentContext(property, value) {
+      return node.context().set(property, value, "file");
+    }
+
+    let active = getPersistentContext("active");
+    let lastMsg = getPersistentContext("lastMsg");
+
+    if (active === undefined) {
+      setPersistentContext("active", false);
+      setPersistentContext("lastMsg", "");
+    } else if (active === true && lastMsg != undefined) {
+      setTimeout(() => {
+        node.emit("restart", lastMsg);
+      }, 500);
+    }
+
+    node.on("input", function (msg, send, done) {
+      if (getPersistentContext("active") === false) {
+        setPersistentContext("active", true);
+      }
+      setPersistentContext("lastMsg", msg);
+
+      send([msg]);
+      done();
+    });
+
+    node.on("restart", function (msg) {
+      node.send([msg]);
+    });
+  }
+
+  RED.nodes.registerType("checkpoint", checkpoint);
 };
