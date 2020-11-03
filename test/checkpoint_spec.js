bec2d07f1caf8983030534062ad498c1e737d2fb
Miguel Pinto <pt.miguel99@gmail.com>
2020-11-03 16:30:59 +0000

    test(checkpoint): Add tests related to local storage

--- test/checkpoint_spec.js
+++ test/checkpoint_spec.js
@@ -32,4 +32,103 @@ describe("checkpoint node", function () {
       }
     });
   });
+
+  it("should clear local context", function (done) {
+    let flow = [
+      {
+        id: "n1",
+        type: "checkpoint",
+        name: "checkpoint",
+      },
+    ];
+
+    helper.load(checkpointNode, flow, function () {
+      let n1 = helper.getNode("n1");
+      n1.emit("reset");
+      try {
+        let active = n1.context().get("active", "file");
+        let lastMsg = n1.context().get("lastMsg", "file");
+        if (active === undefined && lastMsg === undefined) {
+          done();
+        } else {
+          done("Local context was not reset");
+        }
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
+
+  it("should redirect message to output", function (done) {
+    let flow = [
+      {
+        id: "n1",
+        type: "checkpoint",
+        name: "checkpoint",
+        wires: [["n2"]],
+      },
+      {
+        id: "n2",
+        type: "helper",
+        name: "output-from-test-node",
+      },
+    ];
+
+    helper.load(checkpointNode, flow, function () {
+      let n1 = helper.getNode("n1");
+      let n2 = helper.getNode("n2");
+
+      let payload = 42;
+
+      n2.on("input", (msg) => {
+        try {
+          msg.should.have.property("payload", payload);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+
+      n1.receive({ payload: payload });
+    });
+  });
+
+  it("should update local storage", function (done) {
+    let flow = [
+      {
+        id: "n1",
+        type: "checkpoint",
+        name: "checkpoint",
+        wires: [["n2"]],
+      },
+      {
+        id: "n2",
+        type: "helper",
+        name: "output-from-test-node",
+      },
+    ];
+
+    helper.load(checkpointNode, flow, function () {
+      let n1 = helper.getNode("n1");
+      let n2 = helper.getNode("n2");
+
+      let payload = 42;
+
+      n2.on("input", () => {
+        try {
+          let active = n1.context().get("active", "file");
+          let lastMsg = n1.context().get("lastMsg", "file");
+          if (active && lastMsg.payload === 42) {
+            done();
+          } else {
+            done("Local context was not reset");
+          }
+        } catch (err) {
+          done(err);
+        }
+      });
+
+      n1.receive({ payload: payload });
+    });
+  });
 });
