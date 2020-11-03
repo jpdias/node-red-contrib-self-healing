42969e50d2fcb451f73f2e49fe03337307573a21
lilianalmeida <up201706908@fe.up.pt>
2020-11-03 16:08:31 +0000

    feat(resource-monitor): Limit resources usage

--- resource-monitor/resource-monitor.js
+++ resource-monitor/resource-monitor.js
@@ -1,10 +1,61 @@
 module.exports = function (RED) {
+  function checkResource(receivedValue, maxValue, resource, errMsg) {
+    if (receivedValue <= maxValue) {
+      return true;
+    } else {
+      errMsg[resource] = "out of bounds";
+      return false;
+    }
+  }
+
   function ResourceMonitorNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
     node.on("input", function (msg, send, done) {
-      send(msg);
-      done();
+      let returnValue = true;
+      let errMsg = {};
+
+      //if CPU checked confirm if there is a valid value
+      returnValue &= checkResource(
+        msg.payload.CPU,
+        config.maxCPU,
+        "CPU",
+        errMsg
+      );
+
+      //if RAM checked confirm if there is a valid value
+      returnValue &= checkResource(
+        msg.payload.RAM,
+        config.maxRAM,
+        "RAM",
+        errMsg
+      );
+
+      //if Storage checked
+      returnValue &= checkResource(
+        msg.payload.Storage,
+        config.maxStorage,
+        "Storage",
+        errMsg
+      );
+
+      if (returnValue) {
+        node.status({
+          fill: "green",
+          shape: "dot",
+          text: "Within limits",
+        });
+        send([{ payload: "Every resource is within limits." }, null]);
+        done();
+      } else {
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: "Out of bounds",
+        });
+        send([null, { payload: errMsg }]);
+        done(errMsg);
+      }
     });
   }
   RED.nodes.registerType("resource-monitor", ResourceMonitorNode);
