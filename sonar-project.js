8193b4e61115a818b75a35d4e1292cd335b1873e
Miguel Pinto <pt.miguel99@gmail.com>
2020-11-09 17:08:21 +0000

    fix(sonarqube): Remove unnecessary logs

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,6 @@ const sonarqubeScanner = require("sonarqube-scanner");
 let nodePath = process.argv[2];
 
 if (nodePath) {
-  console.log("NODE PATH: " + nodePath);
   sonarqubeScanner(
     {
       serverUrl: "http://localhost:9876",
