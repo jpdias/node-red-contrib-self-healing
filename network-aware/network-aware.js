a8165267296cd4c18097b1b3df6ee5e380a318c3
lilianalmeida <up201706908@fe.up.pt>
2020-11-25 17:17:18 +0000

    refactor(network-aware): Split code into other functions
    Co-authored-by: andremsgomes <andremsgomes@gmail.com>

--- network-aware/network-aware.js
+++ network-aware/network-aware.js
@@ -7,6 +7,47 @@ module.exports = function (RED) {
   let started = false;
   let firstScanComplete = false;
 
+  function getDeviceInfo(obj) {
+    let idsha;
+    let mnf = "unknown";
+    let name = "unknown";
+    let dev;
+
+    if (typeof obj.name == "string") {
+      name = obj.name;
+    }
+
+    if (typeof obj.mac == "string") {
+      idsha = crypto
+        .createHash("sha256")
+        .update(obj.ip + obj.mac)
+        .digest("hex");
+
+      mnf = oui(obj.mac.substring(0, 8));
+
+      if (mnf != "unknown" && mnf != null) {
+        mnf = oui(obj.mac.substring(0, 8)).split("\n")[0];
+      }
+
+      dev = {
+        id: idsha,
+        ip: obj.ip,
+        name: name,
+        manufacturer: mnf,
+        timestamp: new Date().toISOString(),
+      };
+    } else {
+      dev = {
+        ip: obj.ip,
+        name: name,
+        manufacturer: mnf,
+        timestamp: new Date().toISOString(),
+      };
+    }
+
+    return dev;
+  }
+
   function devScan(config, done, node, send) {
     node.status({ fill: "blue", shape: "dot", text: "Scanning..." });
 
@@ -15,49 +56,11 @@ module.exports = function (RED) {
         let newDevList = new Array();
 
         devicesScan.forEach((obj) => {
-          let idsha;
-          let mnf = "unknown";
-          let name = "unknown";
-          let dev;
-
-          if (typeof obj.name == "string") {
-            name = obj.name;
-          }
-
-          if (typeof obj.mac == "string") {
-            idsha = crypto
-              .createHash("sha256")
-              .update(obj.ip + obj.mac)
-              .digest("hex");
-
-            // Look up MAC addresses for their vendor in the IEEE OUI database
-            mnf = oui(obj.mac.substring(0, 8));
-
-            if (mnf != "unknown" && mnf != null) {
-              mnf = oui(obj.mac.substring(0, 8)).split("\n")[0];
-            }
-
-            dev = {
-              id: idsha,
-              ip: obj.ip,
-              name: name,
-              manufacturer: mnf,
-              timestamp: new Date().toISOString(),
-            };
-            send([{ payload: dev }, null]);
-          } else {
-            dev = {
-              ip: obj.ip,
-              name: name,
-              manufacturer: mnf,
-              timestamp: new Date().toISOString(),
-            };
-
-            send([null, { payload: dev }]);
-          }
+          const dev = getDeviceInfo(obj);
           newDevList.push(dev);
         });
 
+        sendDevices(newDevList, send);
         node.context().set("devices", newDevList);
 
         firstScanComplete = true;
@@ -79,6 +82,16 @@ module.exports = function (RED) {
       });
   }
 
+  function sendDevices(scannedDevices, send) {
+    scannedDevices.forEach((device) => {
+      if (device.id) {
+        send([{ payload: device }, null]);
+      } else {
+        send([null, { payload: device }]);
+      }
+    });
+  }
+
   function NetworkAware(config) {
     RED.nodes.createNode(this, config);
     SentryLog.sendMessage("network-aware was deployed");
@@ -87,19 +100,15 @@ module.exports = function (RED) {
     node.on("input", function (msg, send, done) {
       if (!started) {
         devScan(config, done, node, send);
+
         setInterval(() => {
           devScan(config, done, node, send);
         }, parseInt(config.scanInterval) * 1000);
+
         started = true;
       } else if (firstScanComplete) {
         let scannedDevices = node.context().get("devices");
-        scannedDevices.forEach((device) => {
-          if (device.id) {
-            send([{ payload: device }, null]);
-          } else {
-            send([null, { payload: device }]);
-          }
-        });
+        sendDevices(scannedDevices, send);
         done();
       } else {
         node.status({
