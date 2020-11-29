9442c50597381ec264f66782ac17388e836b9b0c
Afoxys <afoxys10@gmail.com>
2020-11-29 18:16:29 +0000

    feat(device-registry): Register and remove devices from current list

--- device-registry/device-registry.js
+++ device-registry/device-registry.js
@@ -9,9 +9,20 @@
     Inputs:
 
     Outputs:
-        Lista de devices
+        Lista de obejtos devices
+          - name
+          - ip
+          - manufacturer
+          - state (on/off/unknown)
+          - lastSeen
         New device
         Device gone
+
+    Adicionar lógica de mensagem mqtt
+
+
+    [ {"deviceId":"1", "deviceName":"FireSensor", "deviceIp":"192.160.111"},
+      {"deviceId":"2", "deviceName":"FireSensor", "deviceIp":"192.160.112"} ]
 */
 
 const SentryLog = require("../utils/sentry-log.js");
@@ -30,6 +41,50 @@ module.exports = function (RED) {
         }
 
     }*/
+  function RegisterDevice(device) {
+    let lastSeen = Date.now().toString();
+
+    if (
+      device.deviceId == null ||
+      device.deviceName == null ||
+      device.deviceIp == null
+    )
+      return false;
+
+    // check if deviceId and deviceIP(?) is on deviceList
+    deviceList.forEach((deviceInList) => {
+      if (
+        deviceInList.deviceId == device.deviceId ||
+        deviceInList.deviceIp == device.deviceIp
+      )
+        return false;
+    });
+
+    deviceList.push({
+      deviceId: device.deviceId,
+      deviceName: device.deviceName,
+      deviceIp: device.deviceIp,
+      deviceStatus: "on",
+      deviceLastSeen: lastSeen,
+    });
+  }
+
+  function RemoveDevice(deviceId) {
+    for (let i = 0; i < deviceList.length; i++) {
+      if (deviceList[i].deviceId == deviceId) {
+        deviceList.splice(i, 1);
+        i--;
+      }
+    }
+  }
+
+  function CheckIdInList(deviceId) {
+    // check if deviceId is on deviceList
+    deviceList.forEach((deviceInList) => {
+      if (deviceInList.deviceId == deviceId) return true;
+    });
+    return false;
+  }
 
   function DeviceRegistry(config) {
     RED.nodes.createNode(this, config);
@@ -38,36 +93,53 @@ module.exports = function (RED) {
     var node = this;
 
     node.on("input", function (msg, send, _done) {
-      let receivedDeviceList = msg.payload;
+      let receivedDevice = msg.payload;
 
-      if (Array.isArray(receivedDeviceList)) {
-        //ver se tem novo elemento
-        receivedDeviceList.forEach((device) => {
-          if (deviceList.indexOf(device) == -1) {
+      if (Array.isArray(receivedDevice)) {
+        receivedDevice.forEach((device) => {
+          if (!CheckIdInList(device.deviceId)) {
             node.status({
               fill: "blue",
               shape: "dot",
               text: "Received new device",
             });
-
+            RegisterDevice(device);
             send([null, { payload: device }, null]);
           }
         });
 
-        //ver se elemento foi removido
-        deviceList.forEach((device) => {
-          if (receivedDeviceList.indexOf(device) == -1) {
+        // TODO: check if new device list has a missing device from deviceList
+        receivedDevice.forEach((device) => {
+          if (CheckIdInList(device.deviceId)) {
             node.status({
               fill: "yellow",
               shape: "dot",
               text: "Removed device",
             });
-
+            RemoveDevice(device.deviceId);
             send([null, null, { payload: device }]);
           }
         });
 
-        deviceList = receivedDeviceList;
+        deviceList = receivedDevice;
+        send({ payload: deviceList }, null, null);
+      } else if (Number.isInteger(receivedDevice)) {
+        if (deviceList.indexOf(receivedDevice) >= 0) {
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: "Device already connected",
+          });
+        } else {
+          node.status({
+            fill: "blue",
+            shape: "dot",
+            text: "Received new device",
+          });
+          deviceList.push(receivedDevice);
+          send([null, { payload: receivedDevice }, null]);
+        }
+
         send({ payload: deviceList }, null, null);
       } else {
         node.status({
