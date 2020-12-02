56cb72470f39a13d842c9941b62f9e2ecc92bfa5
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2020-12-02 18:28:29 +0000

    feat(device-registry): Partial removal of devices

--- device-registry/device-registry.js
+++ device-registry/device-registry.js
@@ -5,7 +5,7 @@ let receivedDevice;
 
 module.exports = function (RED) {
   function registerDevice(device) {
-    let lastSeen = Date.now().toString();
+    //let lastSeen = Date.now();
 
     if (device.Id == null || device.Name == null || device.Ip == null)
       return false;
@@ -13,13 +13,25 @@ module.exports = function (RED) {
     if (device.Status == null) device.Status = "on";
     internalDeviceList.set(device.Id, {
       Id: device.Id,
-      Name: device.Name,
       Ip: device.Ip,
+      //LastSeen: lastSeen,
+      Name: device.Name,
       Status: device.Status,
-      LastSeen: lastSeen,
     });
 
     return true;
+  }
+
+  function unregisterDevice(id) {
+    let device = internalDeviceList.get(id);
+
+    internalDeviceList.set(device.Id, {
+      Id: device.Id,
+      Ip: device.Ip,
+      //LastSeen: lastSeen,
+      Name: device.Name,
+      Status: device.Status,
+    });
   }
 
   function mapToJSON() {
@@ -44,6 +56,8 @@ module.exports = function (RED) {
 
       if (Array.isArray(receivedDevice)) {
         receivedDevice.forEach((device) => {
+          //3 casos: não estar no registo, estar no registo e Status=on/null, estar no registo e Status=off
+
           if (!internalDeviceList.has(device.Id)) {
             if (registerDevice(device)) {
               node.status({
@@ -51,6 +65,7 @@ module.exports = function (RED) {
                 shape: "dot",
                 text: "Registered new device",
               });
+              console.log(device);
               send([null, { payload: device }, null]);
             } else {
               node.status({
@@ -59,7 +74,7 @@ module.exports = function (RED) {
                 text: "Failed to register device",
               });
             }
-          } else {
+          } else if (device.Status != "off") {
             if (registerDevice(device)) {
               node.status({
                 fill: "green",
@@ -73,6 +88,14 @@ module.exports = function (RED) {
                 text: "Failed to update device",
               });
             }
+          } else {
+            unregisterDevice(device.Id);
+            node.status({
+              fill: "yellow",
+              shape: "dot",
+              text: "Removed device",
+            });
+            send([null, null, { payload: device }]);
           }
         });
 
